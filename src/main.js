28fff8d0b770b4816517c09d24886972e112ca02
rashfael <rashfael@isobeef.org>
2019-09-17 21:55:04 +0200

    remove old gridsome code

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import DefaultLayout from '~/layouts/Default.vue'
-
-import './styles/global.styl'
-
-export default function (Vue, { head, router, isServer }) {
-	Vue.component('DefaultLayout', DefaultLayout)
-}
